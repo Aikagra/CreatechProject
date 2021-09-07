18ffdf36915256d95469501e5dd82bf728ec166b
Aikagra Gupta <r22619aikagra@dpsrkp.net>
2021-09-07 10:12:50 +0530

    createch thing

--- app/src/main/java/com/example/techknights/MainActivity.java
+++ app/src/main/java/com/example/techknights/MainActivity.java
@@ -1,14 +1,144 @@
 package com.example.techknights;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText textEmail, textPassword;
+    ProgressBar progressBar;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() !=null)
+        {
+            Intent i = new Intent(MainActivity.this, GroupChatActivity.class);
+            startActivity(i);
+
+        }
+        else{
+
+            textEmail = (TextInputEditText) findViewById(R.id.emailLogin);
+            textPassword = (TextInputEditText) findViewById(R.id.passwordLogin);
+            progressBar = (ProgressBar) findViewById(R.id.progressBarLogin);
+
+            reference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+
+        }
+
+
+
     }
+
+    public void loginUser(View v)
+    {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email = Objects.requireNonNull(textEmail.getText()).toString();
+        String password = Objects.requireNonNull(textPassword.getText()).toString();
+
+        if (!email.equals("") && !password.equals(""))
+        {
+            auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful())
+                                {
+                                    progressBar.setVisibility(View.GONE);
+                                    Toast.makeText(getApplicationContext(), "Logged In Succesfully!", Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(MainActivity.this, GroupChatActivity.class);
+                                    startActivity(i);
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(), "Error Occured, Try Again!", Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.GONE);
+                                }
+
+                        }
+                    });
+        }
+    }
+
+    public void forgotPassword(View view)
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+        LinearLayout container = new LinearLayout(MainActivity.this);
+        container.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams ip = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        ip.setMargins(50, 0, 0, 100);
+
+        final EditText input = new EditText(MainActivity.this);
+        input.setLayoutParams(ip);
+        input.setGravity(Gravity.TOP|Gravity.START);
+        input.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        input.setLines(1);
+        input.setMaxLines(1);
+
+        container.addView(input, ip);
+
+        alert.setMessage("Enter You Registered Email Address");
+        alert.setTitle("Forgot Password");
+        alert.setView(container);
+
+        alert.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                    String entered_email = input.getText().toString();
+
+                    auth.sendPasswordResetEmail(entered_email)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        dialogInterface.dismiss();
+                                        Toast.makeText(getApplicationContext(), "Email Sent! Please Check.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/techknights/Model/AllMethods.java
@@ -0,0 +1,6 @@
+package com.example.techknights.Model;
+
+public class AllMethods
+{
+    public static String name = "";
+}
